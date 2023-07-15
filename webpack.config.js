e9d4654b149ff22dddb970afd1f9c8ae20610c2a
TOBYhhw <1505994122@qq.com>
2023-07-15 17:18:26 +0800

    fix:修复了webpack移除的express服务器路由中间件错误

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,31 @@
 const path = require('path');
-//webpack打包项目的 HtmlWebpackPlugin生成产出HTML文件 user-agent 把浏览器的UserAgent变成一个对象
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: './src/index.js',//入口文件
-    context: process.cwd(),//上下文目录 
-    mode: 'development',//开发模式
+    entry: './src/index.js',
+    context: process.cwd(),
+    mode: 'development',
     output: {
-        path: path.resolve(__dirname, 'dist'),//输出目录
-        filename: 'monitor.js'//文件名
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'monitor.js'
     },
     devServer: {
         static: {
-            directory: path.resolve(__dirname, 'dist'),//devServer静态文件根目录
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        onBeforeSetupMiddleware: function (devServer) {
+            devServer.app.get('/success', function (req, res) {
+                res.json({ id: 1 });
+            });
+            devServer.app.post('/error', function (req, res) {
+                res.sendStatus(500);
+            });
         }
     },
     plugins: [
-        new HtmlWebpackPlugin({//自动打包出HTML文件
+        new HtmlWebpackPlugin({
             template: './src/index.html',
             inject: 'head'
         })
     ]
-}
+};
