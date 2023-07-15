caa192aaa2e106f5b81ae9863376cf2fad766653
TOBYhhw <1505994122@qq.com>
2023-07-15 18:10:01 +0800

    完成白屏错误检测日志上报

--- src/monitor/lib/xhr.js
+++ src/monitor/lib/xhr.js
@@ -3,7 +3,7 @@ export function injectXHR() {
     let XMLHttpRequest = window.XMLHttpRequest;
     let oldOpen = XMLHttpRequest.prototype.open;
     XMLHttpRequest.prototype.open = function (method, url, async) {
-        if (!url.match(/logstores/)) {
+        if (!url.match(/logstores/) && !url.match(/sockjs/)) {
             this.logData = { method, url, async };
         }
         return oldOpen.apply(this, arguments);
